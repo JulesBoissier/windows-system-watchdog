919e743cdedcd5cba952424ac1aee0cd7916a056
Jules Boissier <boissier.jules@gmail.com>
2025-08-25 00:15:02 +0100

    Added FastAPI support

--- main.py
+++ main.py
@@ -1,54 +1,18 @@
-from typing import Dict, List, Any
+import uvicorn
+from fastapi import FastAPI
 
-import win32gui
-import win32con
+from src.windows_utils import get_windows_info
 
+app = FastAPI()
 
-def build_z_index_map() -> Dict[str, int]:
-    """Creates a list of Windows Handle and their associated Z-indices."""
-    z_index_map = {}
-    hwnd = win32gui.GetTopWindow(None)  # topmost window
-    z = 0
-    while hwnd:
-        z_index_map[hwnd] = z
-        hwnd = win32gui.GetWindow(hwnd, win32con.GW_HWNDNEXT)
-        z += 1
-    return z_index_map
+@app.get("/")
+def root():
+    return {"message": "System Watchdog is Running"}
+
+@app.get("/windows_info")
+def windows_info():
+    return get_windows_info()
 
 
-def win_enum_handler(hwnd, ctx):
-    """Handler to be passed to EnumWindows to capture information for visible windows."""
-    if win32gui.IsWindowVisible(hwnd):
-        title = win32gui.GetWindowText(hwnd)
-        rect = win32gui.GetWindowRect(hwnd)
-        width = rect[2] - rect[0]
-        height = rect[3] - rect[1]
-
-        # Append to the passed-in list instead of global
-        ctx.append(
-            {
-                "hwnd": hwnd,
-                "title": title,
-                "rect": rect,
-                "size": (width, height),
-            }
-        )
-
-
-def add_z_index(window_info, z_index_map):
-    """Utility to append the Z-indices to the rest of the window information."""
-    for window in window_info:
-        window["z_index"] = z_index_map[window["hwnd"]]
-    return window_info
-
-
-def get_windows_info() -> List[Dict[str:Any]]:
-    """Returns a list of information for each visible window currently visible."""
-    window_info = []
-
-    win32gui.EnumWindows(win_enum_handler, window_info)
-    z_index_map = build_z_index_map()
-
-    window_info = add_z_index(window_info, z_index_map)
-
-    return window_info
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8002)
